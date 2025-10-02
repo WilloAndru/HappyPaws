6c9c8305c1970abadf03f0375076f89f598a6a81
Willoandru <wilsonandrescriollo@gmail.com>
2025-10-01 19:08:19 -0500

    Barra de categorias

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,13 +2,28 @@ import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import Brand from "./Brand";
 import { IoMenu } from "react-icons/io5";
+import { useEffect, useState } from "react";
+import { getCategories } from "../services/categoriesService";
 
 function Header() {
+  const [isCategories, setIsCategories] = useState(false);
+  const [categories, setCategories] = useState<string[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await getCategories();
+      setCategories(data);
+    })();
+  }, []);
+
   return (
-    <header className="fixed flex justify-between items-center px-3 py-5 md:p-5 bg-cyan-500 w-screen text-white shadow-sm">
+    <header className="fixed flex justify-between items-center px-3 py-5 h-24 md:p-5 bg-cyan-500 w-screen text-white shadow-sm">
       {/* Marca y boton de categorias*/}
       <div className="flex gap-4">
-        <button className="flex flex-col items-center">
+        <button
+          onClick={() => setIsCategories((state) => !state)}
+          className="flex flex-col items-center hover:text-neutral-300"
+        >
           <IoMenu className="text-5xl" />
           <p className="text-[12px]">Categories</p>
         </button>
@@ -29,6 +44,26 @@ function Header() {
       <Link className="btn-1" to="/login">
         Login
       </Link>
+      {/* Interfaz de categorias */}
+      {isCategories && (
+        <div
+          className="fixed left-0 top-24 inset-0 bg-[rgba(0,0,0,0.6)]"
+          onClick={() => setIsCategories(false)}
+        >
+          <div
+            className="absolute bg-cyan-400 h-[60vh] w-64 flex flex-col"
+            onClick={(e) => e.stopPropagation()}
+          >
+            {categories.map((item: string, index: number) => {
+              return (
+                <div className="hover:bg-cyan-500 px-4 py-2" key={index}>
+                  {item}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </header>
   );
 }
