6359a2cd5ed7754f439140813ee1cbe9da9be1be
Willoandru <wilsonandrescriollo@gmail.com>
2025-10-18 09:30:39 -0500

    Estilos de slider de categorias por mascota

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 "use client";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import { FaPause, FaPlay } from "react-icons/fa6";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 export default function Home() {
-  const images = ["/img1.jpg", "/img2.jpg", "/img3.jpg"];
+  const imagesSlider = ["img1.jpg", "img2.jpg", "img3.jpg"];
+  const imagesPets = [
+    "cat.png",
+    "dog.png",
+    "chicken.png",
+    "fish.png",
+    "hamster.png",
+    "horse.png",
+  ];
+
   const [slide, setSlide] = useState(0);
   const [isPauseSlider, setIsPauseSlider] = useState(false);
 
@@ -11,24 +22,25 @@ export default function Home() {
   useEffect(() => {
     if (isPauseSlider) return;
     const interval = setInterval(() => {
-      setSlide((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+      setSlide((prev) => (prev === imagesSlider.length - 1 ? 0 : prev + 1));
     }, 5000);
 
     return () => clearInterval(interval);
   }, [isPauseSlider]);
 
   return (
-    <main className="relative flex gap-2">
-      <section className="rounded-2xl overflow-hidden">
+    <main className="flex gap-6 flex-col">
+      {/* Seccion de imagenes deslisantes */}
+      <section className="relative rounded-2xl overflow-hidden">
         <div
           className="max-h-80 w-full flex transition-transform duration-500 ease-in-out"
           style={{ transform: `translateX(-${slide * 100}%)` }}
         >
-          {images.map((item, index) => {
+          {imagesSlider.map((item, index) => {
             return (
               <img
                 key={index}
-                src={item}
+                src={`/slider/${item}`}
                 alt={`Image ${index}`}
                 className="w-full object-cover"
               />
@@ -42,20 +54,46 @@ export default function Home() {
           >
             {!isPauseSlider ? <FaPause /> : <FaPlay />}
           </button>
-          {images.map((_, index) => {
-            return (
-              <button
-                key={index}
-                className={`w-[12vw] h-2 rounded-4xl ${
-                  slide === index ? "bg-white" : "bg-gray-300"
-                }`}
-                onClick={() => {
-                  setSlide(index);
-                  setIsPauseSlider(true);
-                }}
-              ></button>
-            );
-          })}
+          {imagesSlider.map((_, index) => (
+            <button
+              key={index}
+              className={`w-[12vw] h-2 rounded-4xl ${
+                slide === index ? "bg-white" : "bg-gray-300"
+              }`}
+              onClick={() => {
+                setSlide(index);
+                setIsPauseSlider(true);
+              }}
+            ></button>
+          ))}
+        </div>
+      </section>
+      {/* Seccion de categorias por mascota */}
+      <section>
+        <h2>What’s your pet?</h2>
+        <div className="relative flex items-center mt-2 overflow-hidden">
+          <button className="fixed left-5 bg-primary rounded-full p-4 text-white hover:bg-primary-hover">
+            <FaArrowLeft />
+          </button>
+
+          {imagesPets.map((item, index) => (
+            <Link
+              className="flex flex-col items-center gap-2 hover:bg-gray-200 rounded-2xl p-2"
+              key={item}
+              href={item}
+            >
+              <img
+                src={`/categories/${item}`}
+                alt={`Image ${index}`}
+                className="min-w-32 object-cover rounded-full flex-shrink-0"
+              />
+              <h6>{item}</h6>
+            </Link>
+          ))}
+
+          <button className="fixed right-5 bg-primary rounded-full p-4 text-white hover:bg-primary-hover">
+            <FaArrowRight />
+          </button>
         </div>
       </section>
     </main>
