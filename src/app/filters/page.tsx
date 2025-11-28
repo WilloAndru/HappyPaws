ac93ea68594e9670ed72fe25d6504726f47ef16b
Willoandru <wilsonandrescriollo@gmail.com>
2025-11-28 10:05:17 -0500

    Adelanto logica filtrado

--- src/app/filters/page.tsx
+++ src/app/filters/page.tsx
@@ -10,45 +10,48 @@ import Link from "next/link";
 export default function Filters() {
   const searchParams = useSearchParams();
   const search = searchParams.get("search") || ""; //String a buscar
-  const [animalType, setAnimalType] = useState<string | null>(
-    searchParams.get("animalType")
-  ); //Tipo de animal a filtrar
-  const [category, setCategory] = useState<string | null>(
-    searchParams.get("category")
-  ); //Categoria a filtrar
 
-  // Listas de elementos de los selects
+  // Estados y elementos de los filtros
   const [animalTypes, setAnimalTypes] = useState([]);
+  const [animalType, setAnimalType] = useState<any>();
   const [categories, setCategories] = useState([]);
+  const [category, setCategory] = useState<any>();
   const optionsPrice = [
     { value: 0, label: "Lowest price first" },
     { value: 1, label: "Highest price first" },
   ];
-
-  // Obtenemos todos los productos indicados
-  const { data } = useSearchProducts(search, animalType, category, 50);
-  console.log(data);
+  const [orderPrice, setOrderPrice] = useState<any>();
 
   // Obtenemos los datos de las categorias para los filtros
   useEffect(() => {
     const getCategoriesForFilters = async () => {
-      // Funcion para capitalizar los label
-      const capitalize = (text: string) =>
-        text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
-
       try {
         const res = await axios.get("/api/categories");
+        // Guardamos todos los tipos de animales
         setAnimalTypes(
           res.data.animalTypes.map((item: string, index: number) => ({
             value: index,
-            label: capitalize(item),
+            label: item,
           }))
         );
+        // Guardamos el tipo de animal actual a filtrar
+        setAnimalType(
+          res.data.animalTypes.find(
+            (a: any) => a.label === searchParams.get("animalType")
+          )
+        );
+        // Guardamos todas las categorias
         setCategories(
           res.data.categories.map((item: string, index: number) => ({
             value: index,
-            label: capitalize(item),
+            label: item,
           }))
+        );
+        // Guardamos la categoria actual a filtrar
+        setCategory(
+          res.data.categories.find(
+            (a: any) => a.label === searchParams.get("category")
+          )
         );
       } catch (error) {
         console.error("Error", error);
@@ -57,37 +60,65 @@ export default function Filters() {
     getCategoriesForFilters();
   }, []);
 
+  // Obtenemos todos los productos indicados
+  const { data } = useSearchProducts(
+    search,
+    searchParams.get("animalType"),
+    searchParams.get("category"),
+    50
+  );
+  const [filteredData, setFilteredData] = useState(data?.results || []);
+  useEffect(() => {
+    setFilteredData(data?.results || []);
+  }, [data]);
+
+  // Aplicar filtros
+  const handleFilter = () => {};
+
+  // Limpiar filtros
+  const handleClearFilter = () => {
+    setAnimalType(null);
+    setCategory(null);
+    setOrderPrice(null);
+  };
+
   return (
     <main className="flex gap-4 flex-col md:flex-row">
       {/* Seccion de filtros */}
-      <section className="sticky top-25 bg-gray-200 rounded-xl px-5 py-3 md:w-1/3 flex flex-col gap-4 h-fit">
+      <section className="md:sticky md:top-25 bg-gray-200 rounded-xl px-5 py-3 md:w-1/3 flex flex-col gap-4 h-fit">
         {/* Filtro por precio */}
         <div className="flex gap-2 flex-col">
           <h5>Price</h5>
-          <Select options={optionsPrice} />
+          <Select options={optionsPrice} onChange={setOrderPrice} />
         </div>
         {/* Filtro por tipo de animal */}
         <div className="flex gap-2 flex-col">
           <h5>Animal type</h5>
-          <Select options={animalTypes} />
+          <Select options={animalTypes} onChange={setAnimalType} />
         </div>
         {/* Filtro por categoria */}
         <div className="flex gap-2 flex-col">
           <h5>Category</h5>
-          <Select options={categories} />
+          <Select options={categories} onChange={setCategory} />
         </div>
         {/* Botones de filtrar y limpiar filtro */}
         <div className="flex gap-2 mb-2">
-          <button className="rounded-xl text-white px-4 py-2 bg-primary hover:bg-primary-hover w-1/2">
+          <button
+            onClick={handleFilter}
+            className="rounded-xl text-white px-4 py-2 bg-primary hover:bg-primary-hover w-1/2"
+          >
             Apply filters
           </button>
-          <button className="rounded-xl text-white px-4 py-2 bg-blue-400 hover:bg-blue-500 w-1/2">
+          <button
+            onClick={handleClearFilter}
+            className="rounded-xl text-white px-4 py-2 bg-blue-400 hover:bg-blue-500 w-1/2"
+          >
             Clear filters
           </button>
         </div>
       </section>
       {/* Seccion de paginacion */}
-      <section className="bg-gray-200 rounded-xl px-5 py-3 flex flex-col gap-2 md:w-2/3">
+      <section className="bg-gray-200 rounded-xl px-5 py-3 flex flex-col gap-3 md:w-2/3">
         {/* Header paginacion */}
         <header className="flex justify-between">
           {/* Titulo de que se esta buscando por caracteres */}
@@ -97,15 +128,19 @@ export default function Filters() {
           {/* Indicadores de que se esta filtrando */}
           <div className="flex gap-3">
             {animalType && (
-              <h6 className="px-3 py-2 bg-gray-100 rounded-xl">{animalType}</h6>
+              <h6 className="px-3 py-2 bg-gray-100 rounded-xl">
+                {animalType.label}
+              </h6>
             )}
             {category && (
-              <h6 className="px-3 py-2 bg-gray-100 rounded-xl">{category}</h6>
+              <h6 className="px-3 py-2 bg-gray-100 rounded-xl">
+                {category.label}
+              </h6>
             )}
           </div>
         </header>
         {/* Lista de productos */}
-        {data?.results.map((item: any) => (
+        {filteredData?.map((item: any) => (
           <Link
             href={`/product/${item.id}`}
             key={item.id}
